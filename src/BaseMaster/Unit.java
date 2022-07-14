ffb9f864b55e9e6113f5b2542000ff62000cb159
Sivathanu S <sivathanu1420@gmail.com>
2022-07-14 10:30:15 +0530

    Create Unit.java

---
+++ src/BaseMaster/Unit.java
@@ -0,0 +1,20 @@
+package BaseMaster;
+
+public class Unit {
+	
+	public static void unitAdd()
+	{
+		
+	}
+	
+	public static void unitEdit()
+	{
+		
+	}
+	
+	public static void unitDelete()
+	{
+		
+	}
+
+}
